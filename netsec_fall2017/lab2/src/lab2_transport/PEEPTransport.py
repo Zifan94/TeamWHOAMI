d262ff38bd2184116529440e7d6ccd9058ba0dfe
ELROY-PC\hasee <782172672@qq.com>
2017-10-10 11:12:52 -0400

    retransmission_timeout

--- netsec_fall2017/lab2/src/lab2_transport/PEEPTransport.py
+++ netsec_fall2017/lab2/src/lab2_transport/PEEPTransport.py
@@ -3,11 +3,13 @@ from playground.network.packet.fieldtypes import UINT32, UINT16, UINT8, STRING, 
 from playground.network.common import StackingProtocol, StackingTransport, StackingProtocolFactory
 from ..lab2_packets import *
 from ..lab2_Util import *
+import asyncio
 
-DATA_CHUNK_SIZE = 10
+DATA_CHUNK_SIZE = 20
 class PEEPTransport(StackingTransport):
+	TIME_OUT_LIMIE = 3
 	logging = True
-	PEEPPacketList = []
+	RetransmissionPacketList = {0: ""}
 	sequenceNumber = 0
 
 	def write(self, data):
@@ -23,25 +25,31 @@ class PEEPTransport(StackingTransport):
 		if self.logging:
 			print("\nPEEP Transport: data length is [%s], and divided into [%s] PEEP packets"%(len(data), size))
 
-		CurrentPEEPPacketList = []
 		for i in range(1, size+1):
 			if self.logging:
 				print("PEEP Transport: packing #%s PEEP packet..."%i)
 			cur_Data_Chuck = (data[(i-1)*DATA_CHUNK_SIZE : i*DATA_CHUNK_SIZE])
 			cur_PEEP_Packet = Util.create_outbound_packet(5, self.sequenceNumber, None, cur_Data_Chuck)
-			self.sequenceNumber = self.sequenceNumber+len(cur_Data_Chuck)
-			CurrentPEEPPacketList.append(cur_PEEP_Packet)
-			self.PEEPPacketList.append(cur_PEEP_Packet)
-		# #create PEEPPacket
-		for pkt in CurrentPEEPPacketList:
-			self.lowerTransport().write(pkt.__serialize__())
-
+			self.sequenceNumber = self.sequenceNumber + len(cur_Data_Chuck)
+			self.RetransmissionPacketList.update({self.sequenceNumber: cur_PEEP_Packet})
+			self.lowerTransport().write(cur_PEEP_Packet.__serialize__())
+			##############TIME_OUT CHECK################
+			asyncio.get_event_loop().call_later(self.TIME_OUT_LIMIE, self.retransmission_checker, self.sequenceNumber)
 		if self.logging:
-			print("PEEP Transport: [%s] PEEP Packets written!\n"%len(CurrentPEEPPacketList))
+			print("PEEP Transport: [%s] PEEP Packets written!\n"%size)
 
-		
+	def clean_databuffer(self):  # send the rest data buffer in the list
+		print()
+
+	def retransmission_checker(self,seq):
+		if seq in self.RetransmissionPacketList:
+			if self.logging:
+				print("PEEP Transport: Packets ack = [%s] not received after TIMEOUT, Retransmission...." %seq)
+			self.lowerTransport().write(self.RetransmissionPacketList[seq].__serialize__())
 
 	def ack_received(self,ack):
 		if self.logging:	print("PEEP Transport: ACK received, Ack = %d" % ack)
+		del self.RetransmissionPacketList[ack]
+
 
 
