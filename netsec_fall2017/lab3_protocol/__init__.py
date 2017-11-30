b34a10b4c47f8a89f441a259e997655349449ce1
zifan <mikeyang1625@gmail.com>
2017-11-29 23:20:05 -0500

    update inti stacking-> PEEP, PLS

--- netsec_fall2017/lab3_protocol/__init__.py
+++ netsec_fall2017/lab3_protocol/__init__.py
@@ -7,8 +7,8 @@ from .src.lab3_protocol import *
 from .lab2_protocol.src.lab2_protocol import *
 from playground.network.common import StackingProtocolFactory
 
-cf = StackingProtocolFactory(lambda: PLSClientProtocol(), lambda: PEEPClientProtocol(logging = False))
-sf = StackingProtocolFactory(lambda: PLSServerProtocol(), lambda: PEEPServerProtocol(logging = False))
+cf = StackingProtocolFactory(lambda: PEEPClientProtocol(logging = True), lambda: PLSClientProtocol())
+sf = StackingProtocolFactory(lambda: PEEPServerProtocol(logging = True), lambda: PLSServerProtocol())
 
 lab_connector = playground.Connector(protocolStack=(cf, sf))
 playground.setConnector('lab3_protocol', lab_connector)
